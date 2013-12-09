dd17965b3d44aa4ef53864eefde789472bf3893c
Mattias Wolff <mattias.wolff@gmail.com>
2013-12-09 23:23:26 +0100

    Lots of stuff

--- eventPage.js
+++ eventPage.js
@@ -1,42 +1,99 @@
-var url = localStorage["mediusflow_url"];
-var username = localStorage["mediusflow_username"];
-var password = localStorage["mediusflow_password"];
+function getMediusflowUrl() {
+  return localStorage["mediusflow_url"];
+}
 
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  chrome.tabs.create({ url: url });
-});
+function getLoadTaskUrl() {
+  return getMediusflowUrl() + "/Backend/Rest/InboxManager/Tasks/1";
+}
 
-var loadTasksUrl = url + "/Backend/Rest/InboxManager/Tasks/1"
-var loginUrl = url + "/LocalAccount/LogOn"
-var loginBody = "a=send&username=" + username + "&password=" + password
+function goToInbox() {
+  console.log("MediusflowURL: " + getMediusflowUrl());
+  if (getMediusflowUrl()) {
+    chrome.tabs.create({ url: getMediusflowUrl() });
+  }
+  else {
+    console.log("No Url");
+    chrome.tabs.create({ url: '/options.html' });
+  }
+}
 
-console.log (loadTasksUrl);
-console.log (loginUrl);
+function scheduleGetMediusflowData(){
+ chrome.alarms.get("GetDataFromMediusflow",function(alarm){
+  console.log("Scheduled Time (before)  "+ alarm.scheduledTime);
+  console.log("Alarm Name "+alarm.name);
+ });
 
-$.ajax({
-  contentType: "application/x-www-form-urlencoded",
-  url: loadTasksUrl,
-  data: loginBody
-}).done(function() {
-    console.log( "Login successful");
-  })
-  .fail(function() {
-    console.log( "Login failed");
-  });
+ chrome.alarms.create("GetDataFromMediusflow",{periodInMinutes:10});
+ 
+ chrome.alarms.get("GetDataFromMediusflow",function(alarm){
+  console.log("Scheduled Time (after) "+ alarm.scheduledTime);
+  console.log("Alarm Name "+alarm.name);
+ });
+}
 
-$.ajax({
-  dataType: "json",
-  url: loadTasksUrl
-}).done(function(data) {
-	var countTasks = 0;
-	$.each(data, function(i, item) {
-		countTasks = countTasks + item.TotalTasks;
-		console.log (item.TotalTasks);
-		console.log (countTasks);
-	});
-	chrome.browserAction.setBadgeText({text : countTasks.toString()});
-  })
-  .fail(function() {
+function getMediusflowData(params) {
+  if (params && params.scheduleGetMediusflowData) scheduleGetMediusflowData();
+  $.ajax({
+    dataType: "json",
+    url: getLoadTaskUrl()
+  }).done(function(data) {
+    var countTasks = 0;
+    $.each(data, function(i, item) {
+      countTasks = countTasks + item.TotalTasks;
+    });
+    console.log("Total count: " + countTasks.toString());
+    chrome.browserAction.setBadgeText({text : countTasks.toString()});
+  }).fail(function() {
+    console.log("Count failed..");
     chrome.browserAction.setBadgeText({text:"?"});
   });
 
+}
+
+function isMediusflowUrl(url) {
+// Return whether the URL starts with the Gmail prefix.
+  return url.indexOf(getMediusflowUrl()) == 0;
+}
+
+function onNavigate(details) {
+  if (details.url && isMediusflowUrl(details.url)) {
+    console.log('Recognized Mediusflow navigation to: ' + details.url + '.' +
+      'Refreshing count...');
+    getMediusflowData({scheduleGetMediusflowData:false});
+  }
+}
+
+function onInit() {
+  console.log('onInit');
+  getMediusflowData({scheduleGetMediusflowData:true});
+}
+
+function onAlarm(alarm) {
+  console.log('Got alarm', alarm);
+  getMediusflowData({scheduleGetMediusflowData:true});
+}
+
+if (chrome.webNavigation && chrome.webNavigation.onDOMContentLoaded && 
+  chrome.webNavigation.onReferenceFragmentUpdated) {
+    chrome.webNavigation.onDOMContentLoaded.addListener(onNavigate, filters);
+    chrome.webNavigation.onReferenceFragmentUpdated.addListener(onNavigate, filters);
+  } else {
+    chrome.tabs.onUpdated.addListener(function(_, details) {
+      onNavigate(details);
+    });
+  }
+
+chrome.runtime.onInstalled.addListener(onInit);
+chrome.alarms.onAlarm.addListener(onAlarm);
+chrome.browserAction.onClicked.addListener(goToInbox);
+
+if (chrome.runtime && chrome.runtime.onStartup) {
+  chrome.runtime.onStartup.addListener(function() {
+    console.log('Starting browser... updating icon.');
+    startRequest({scheduleRequest:false, showLoadingAnimation:false});
+  });
+}
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+  console.log("Run GetDataFromMediusflow...");
+});
--- options.js
+++ options.js
@@ -1,30 +1,18 @@
 // Saves options to localStorage.
 function save_options() {
   var url = document.getElementById("url").value;
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
   localStorage["mediusflow_url"] = url;
-  localStorage["mediusflow_username"] = username;
-  localStorage["mediusflow_password"] = password;
+
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
-}
+  status.innerHTML = "Options Saved. Please start using the extension!";
+  }
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
   var url = localStorage["mediusflow_url"];
-  var username = localStorage["mediusflow_username"];
-  var password = localStorage["mediusflow_password"];
-  
-  document.getElementById("url").value = url;
-  document.getElementById("username").value = username;
-  document.getElementById("password").value = password;
-  
+  document.getElementById("url").value = url;  
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
